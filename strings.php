86ec601394f72084f73c19209f9055b5d80c6b82
ThiagoEXE <your-email-addressthiagob.oliveira@ucsal.edu.br>
2022-05-08 17:07:35 -0300

    Usando Strings e o comando .PHP_EOL para pular linha

---
+++ strings.php
@@ -0,0 +1,17 @@
+<?php
+
+$idade = 25;
+/*Para concatenar strings com variaveis no php 
+se utiliza "." "ponto" */
+
+/*---------------------------------------------------------------*/
+
+ /*Diferença na sintaxe quando se usa apostófro 
+ou áspas simples é preciso usar o "." ponto para concatenar uma string com uma variável*/
+echo "Olá mundo!" . PHP_EOL;//Comando PHP_EOL serve para pular uma linha
+
+/*Quando se usa áspas duplas não é preciso usar ponto para a concatenação da string*/
+echo "Eu tenho $idade anos"; 
+
+// \n é usadpo para pular linha no php
+// \t é usado para recúo como a tecla tab
