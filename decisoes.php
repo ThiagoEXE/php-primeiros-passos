a4c5ba15c50404457042d941ea583c04c88ff4aa
ThiagoEXE <thiagob.oliveira@ucsal.edu.br>
2022-05-09 17:37:33 -0300

    Estruturas de repetição

--- decisoes.php
+++ decisoes.php
@@ -1,17 +1,18 @@
 <?php
 
-$idade = 26;
-//$nome = 'Thiago';
+$idade = 18;
+$numeroDePessoas = 2;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou";
 echo "a partir de 16 anos acompanhado".PHP_EOL;
 
 if ($idade >= 18){
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo 'Pode entar';
-} else {
-    echo "Você só tem $idade anos. \nVocê não pode entrar";
-}
+    echo "Você tem $idade anos. Pode entar";
+} else if($idade >= 16 && $numeroDePessoas > 1){
+    echo "Você só tem $idade anos, e está acompanhado(a), então pode entrar.";
+}else
+    echo "Você só tem $idade, anos. Você não pode entrar."; 
+
 
 echo PHP_EOL;
 echo "Adeus!";
