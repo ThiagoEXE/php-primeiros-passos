c12b31bb98d8b6c52da3b55add94c1321b7887d7
ThiagoEXE <your-email-addressthiagob.oliveira@ucsal.edu.br>
2022-05-12 21:22:37 -0300

    alterações locais

--- decisoes.php
+++ decisoes.php
@@ -1,17 +1,25 @@
 <?php
 
-$idade = 26;
-//$nome = 'Thiago';
+$idade = 17;
+$numeroDePessoas = 1;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou";
-echo "a partir de 16 anos acompanhado".PHP_EOL;
+echo " a partir de 16 anos acompanhado".PHP_EOL;
 
 if ($idade >= 18){
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo 'Pode entar';
-} else {
-    echo "Você só tem $idade anos. \nVocê não pode entrar";
-}
+
+    echo "Você tem $idade anos. Pode entar sozinho";
+
+} else if($idade >= 16 && $numeroDePessoas > 1){
+
+        echo "Você tem $idade anos, está acompanhado(a), então pode entrar";
+
+    }else{
+
+        echo "Você só tem $idade anos e não está acompanhado(a). \nVocê não pode entrar";
+
+    }
+
 
 echo PHP_EOL;
 echo "Adeus!";
