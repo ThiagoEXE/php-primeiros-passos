2fd784d6626ee6869272fa950910894139f3c2d8
ThiagoEXE <thiagob.oliveira@ucsal.edu.br>
2022-05-17 17:38:11 -0300

    Trabalhando com funções

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,5 +1,9 @@
 <?php
 
+function exibeMensagem($mensagem){
+    echo $mensagem . PHP_EOL;
+}
+
 $contasCorrentes = [
     '123.456.789=10' => [
         'titular' => 'Alaberto',
@@ -15,8 +19,20 @@ $contasCorrentes = [
     ]
 ];
 //Realizando saque de 500$ na conta de Thiago
-$contasCorrentes ['123.756.789-11'] ['saldo'] -= 500;
+//$contasCorrentes ['123.756.789-11'] ['saldo'] -= 500;
+
+if(500 > $contasCorrentes ['123.456.789=10'] ['saldo']){
+    exibeMensagem("Você não pode sacar este valor");
+}else{
+    $contasCorrentes ['123.456.789=10'] ['saldo']-= 500;
+}
+
+if(500 > $contasCorrentes['123.459.789-18']['saldo']){
+    exibeMensagem("Você não pode sacar este valor");
+}else{
+    $contasCorrentes['123.756.789-11']['saldo'] -= 500;
+}
 
 foreach ($contasCorrentes as $cpf => $conta){
-    echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
 }
