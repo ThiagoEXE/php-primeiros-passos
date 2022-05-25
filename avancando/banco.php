129867826d0257f3e197eab9c973228545ba1e4e
ThiagoEXE <thiagob.oliveira@ucsal.edu.br>
2022-05-25 14:06:31 -0300

    Trabalhanco com funções

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,12 +1,22 @@
 <?php
 
+function sacar($conta, $valorASacar){
+
+    if($valorASacar > $conta ['saldo']){
+        exibeMensagem("Você não pode sacar");
+    }else{
+        $conta ['saldo']-= $valorASacar;
+    }
+    return $conta;
+}
+
 function exibeMensagem($mensagem){
     echo $mensagem . PHP_EOL;
 }
 
 $contasCorrentes = [
-    '123.456.789=10' => [
-        'titular' => 'Alaberto',
+    '123.456.789-10' => [
+        'titular' => 'Alberto',
         'saldo' => 300
     ], 
     '123.459.789-18' => [
@@ -18,20 +28,11 @@ $contasCorrentes = [
         'saldo' => 1000
     ]
 ];
+
+$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], valorASacar: 500);
+$contasCorrentes['123.459.789-18'] = sacar($contasCorrentes['123.459.789-18'], valorASacar: 500);
 //Realizando saque de 500$ na conta de Thiago
 //$contasCorrentes ['123.756.789-11'] ['saldo'] -= 500;
-
-if(500 > $contasCorrentes ['123.456.789=10'] ['saldo']){
-    exibeMensagem("Você não pode sacar este valor");
-}else{
-    $contasCorrentes ['123.456.789=10'] ['saldo']-= 500;
-}
-
-if(500 > $contasCorrentes['123.459.789-18']['saldo']){
-    exibeMensagem("Você não pode sacar este valor");
-}else{
-    $contasCorrentes['123.756.789-11']['saldo'] -= 500;
-}
 
 foreach ($contasCorrentes as $cpf => $conta){
     exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
